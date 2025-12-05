5d2ed25fe39d55526510fbbefbc2edff3bb01861
潘逸飞 <yifei.pan1@geely.com>
2025-12-05 09:28:32 +0800

    update

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/EvanPan1997/go-grammar-analysis/grammar"
+	"github.com/antlr4-go/antlr/v4"
 )
 
 func main() {
 	fmt.Println("Hello")
+
+	input := antlr.NewInputStream("SELECT * FROM TABLE_NAME")
+
+	lexer := grammar.NewUniversalLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	parser := grammar.NewUniversalParser(stream)
+	parser.BuildParseTrees = true
+	statements := parser.Statements()
+	statement := statements.Statement(0)
+	fmt.Println(statement.GetText())
 }
